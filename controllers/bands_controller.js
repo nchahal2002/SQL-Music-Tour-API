ff34c5d5cf41a32b30d2f43f1d0b9dd3cdb6ee52
Nihal Chahal <nchahal2002@gmail.com>
2024-01-10 14:46:17 -0600

    made changes to file

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -38,5 +38,37 @@ bands.post('/', async (req, res) => {
     }
 })
 
+// UPDATE A BAND
+bands.put('/:id', async (req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 // EXPORT
 module.exports = bands
