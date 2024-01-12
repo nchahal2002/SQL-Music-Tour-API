526c096157f65c532550f8247d70a21fae3e6b71
Nihal Chahal <nchahal2002@gmail.com>
2024-01-12 10:23:49 -0600

    made changes to file

--- models/meetgreet.js
+++ models/meetgreet.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class MeetGreet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Band, Event }) {
       // band
       MeetGreet.belongsTo(Band, {
